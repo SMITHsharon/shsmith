460e29c31fd2c5f83a6cf034eced2c2c8c3517ad
sharon smith <sharon.e.smith2@gmail.com>
2017-11-08 01:28:12 -0600

    formatting (Cards) and content changes and additions (mostly Experience + profile image)

--- javascripts/controllers/SmithExpCtrl.js
+++ javascripts/controllers/SmithExpCtrl.js
@@ -1,9 +1,14 @@
 app.controller("SmithExpCtrl", function($location, $scope) {
 
 $scope.expHats = false;
+$scope.MLKMission - false;
 
 $scope.showHats = () => {
 		$scope.expHats = true;
+	};
+
+$scope.showMLK = () => {
+		$scope.MLKMission = true;
 	};
 
 /*
