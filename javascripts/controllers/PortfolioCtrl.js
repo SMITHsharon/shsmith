e330f25c1d566370b1aeb4aa844b42b28ec056ae
sharon smith <sharon.e.smith2@gmail.com>
2017-11-29 22:53:49 -0600

    add portfolio photo image; clean link functionality

--- javascripts/controllers/PortfolioCtrl.js
+++ javascripts/controllers/PortfolioCtrl.js
@@ -3,6 +3,7 @@ app.controller("PortfolioCtrl", function($scope, $location) {
 	$scope.techCommonMeals = false;
 	$scope.techBackEnd = false;
 	$scope.techWYSIWYG = false;
+	$scope.techAddressBook = false;
 	$scope.techThisSite = false;
 
 	$scope.showCommonMeals = () => {
@@ -19,6 +20,10 @@ app.controller("PortfolioCtrl", function($scope, $location) {
 
 	$scope.showAddressBook = () => {
 		$scope.techAddressBook = $scope.techAddressBook === false ? true: false;
+	};
+
+	$scope.showThisSite = () => {
+		$scope.techThisSite = $scope.techThisSite === false ? true: false;
 	};
     
 
