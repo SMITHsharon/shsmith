3821dc9bbe715213768430bfde29e5c762eb688c
sharon smith <sharon.e.smith2@gmail.com>
2017-12-30 16:05:40 -0600

    more content tweaks

--- javascripts/controllers/SmithExpCtrl.js
+++ javascripts/controllers/SmithExpCtrl.js
@@ -1,10 +1,8 @@
 app.controller("SmithExpCtrl", function($location, $scope) {
 
 	$scope.expHats = false;
-	//$scope.MLKMission = false;
 
 	$scope.showHats = () => {
-		//$scope.expHats = true;
 		$scope.expHats = $scope.expHats === false ? true: false;
 	};
 
