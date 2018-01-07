3583ea4606c48016384f523629ecf05c8ab32280
Zoe Ames <zoeames@gmail.com>
2018-01-07 10:16:53 -0600

    updated gruntfile

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -1,3 +1,4 @@
+'use strict';
 app.config(function($routeProvider) {
   $routeProvider
     .when('/portfolio', {
--- javascripts/controllers/SmithSmartsCtrl.js
+++ javascripts/controllers/SmithSmartsCtrl.js
@@ -1,9 +1,9 @@
+'use strict';
 app.controller("SmithSmartsCtrl", function($location, $scope) {
 
 	$scope.showNSSDesc = false;
 
 	$scope.showAboutNSS = () => {
-		console.log("toggling");
 		$scope.showNSSDesc = $scope.showNSSDesc === false ? true: false;
 	};
 
